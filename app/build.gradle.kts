5423403753dd62a23de600450923b8ffaf97946a
RAZAAli901 <razaalymurtaza@gmail.com>
2026-08-14 01:44:13 +0500

    Step 6: Add Compose BOM and core Compose UI dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,11 +53,14 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+
+    // Section B Step 6: Compose BOM & Core Compose UI Dependencies
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
