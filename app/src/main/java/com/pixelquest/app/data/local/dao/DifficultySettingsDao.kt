7728b36e49ce56db379db4c2e0d084c873b676bc
RAZAAli901 <razaalymurtaza@gmail.com>
2026-08-18 01:35:52 +0500

    Step 23: Add getCurrentDifficulty query method to DifficultySettingsDao

--- app/src/main/java/com/pixelquest/app/data/local/dao/DifficultySettingsDao.kt
+++ app/src/main/java/com/pixelquest/app/data/local/dao/DifficultySettingsDao.kt
@@ -3,8 +3,10 @@ package com.pixelquest.app.data.local.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import androidx.room.Update
 import com.pixelquest.app.data.local.entity.DifficultySettingsEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface DifficultySettingsDao {
@@ -13,4 +15,7 @@ interface DifficultySettingsDao {
 
     @Update
     suspend fun updateSettings(settings: DifficultySettingsEntity)
+
+    @Query("SELECT * FROM difficulty_settings WHERE id = 1")
+    fun getCurrentDifficulty(): Flow<DifficultySettingsEntity?>
 }
