c314041657487c4b817c7c4ca1bdbd2123922a3b
RAZAAli901 <razaalymurtaza@gmail.com>
2026-09-03 02:32:32 +0500

    Remove kapt correctErrorTypes to resolve Could not load module in Kotlin 1.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,10 +68,6 @@ android {
     }
 }
 
-kapt {
-    correctErrorTypes = true
-}
-
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
